bf0ecc5303c2e7047e07ebad0a878cd9b3177469
Karl Sakas <karldonus@gmail.com>
2015-10-02 12:09:58 -0400

    modified map json request

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-
-    var url = "https://api.mapbox.com/v4/geocode/mapbox.places/2306 Rivermont Avenue,Lynchburg,Virginia.json?access_token=pk.eyJ1Ijoia2FybGRvbnVzIiwiYSI6Ijg1ZTY5ZDZjMTUxZTdkMzk1Y2MwOTNjNjQwZDMwNTU2In0.WOrmvw7P5KviJbR_u5febw";
+    var address = rentalAddress + "," + rentalCity + "," + rentalState;
+    alert(address);
+    var url = "https://api.mapbox.com/v4/geocode/mapbox.places/" + address + ".json?access_token=pk.eyJ1Ijoia2FybGRvbnVzIiwiYSI6Ijg1ZTY5ZDZjMTUxZTdkMzk1Y2MwOTNjNjQwZDMwNTU2In0.WOrmvw7P5KviJbR_u5febw";
 
       $.getJSON(url).then(function(response){
         var long = response.features[0].center[0];
@@ -9,7 +10,7 @@ $(document).ready(function(){
     // Provide your access token
       L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGRvbnVzIiwiYSI6Ijg1ZTY5ZDZjMTUxZTdkMzk1Y2MwOTNjNjQwZDMwNTU2In0.WOrmvw7P5KviJbR_u5febw';
     // Create a map in the div #map
-      var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 13);
+      var map = L.mapbox.map('map', 'mapbox.streets').setView([lat, long], 15);
         L.marker([lat, long], {
           icon: L.mapbox.marker.icon({
             'marker-size': 'large',
